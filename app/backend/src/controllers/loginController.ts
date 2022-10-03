b8b57790bb867728a658afd9fbdad089016f9c31
Maria Isabella Miranda da Silva <ma_isabella.miranda@hotmail.com>
2022-10-03 19:50:58 -0300

    feat: Criada classe para servir de controller da aplicação no rota /login

---
+++ app/backend/src/controllers/loginController.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from 'express';
+import LoginService from '../services/LoginService';
+
+export default class LoginController {
+  constructor(private loginService: LoginService) { }
+
+  async loginValidate(req: Request, res: Response) {
+    const { email, password } = req.body;
+
+    const { code, data, message } = await this.loginService.verifyLogin({ email, password });
+
+    if (!data) return res.status(code).json(message);
+
+    res.status(code).json(data);
+  }
+}
