b33af16e783acd65a35fa587ab8d991f1ac48542
Maria Isabella Miranda da Silva <ma_isabella.miranda@hotmail.com>
2022-10-03 19:52:58 -0300

    feat: Criada model do sequelize para tabela matches

---
+++ app/backend/src/database/models/Matche.ts
@@ -0,0 +1,65 @@
+import { Model, INTEGER, BOOLEAN } from 'sequelize';
+import db from '.';
+import Team from './Team';
+
+class Matche extends Model {
+  public id!: number;
+  public username!: string;
+  public role!: number;
+  public email!: string;
+  public password!: string;
+}
+
+Matche.init({
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: INTEGER,
+  },
+  homeTeam: {
+    type: INTEGER,
+    allowNull: false,
+    references: {
+      model: 'teams',
+      key: 'id',
+    },
+    onUpdate: 'CASCADE',
+    onDelete: 'CASCADE',
+  },
+  homeTeamGoals: {
+    allowNull: false,
+    type: INTEGER,
+  },
+  awayTeam: {
+    allowNull: false,
+    type: INTEGER,
+    references: {
+      model: 'teams',
+      key: 'id',
+    },
+    onUpdate: 'CASCADE',
+    onDelete: 'CASCADE',
+  },
+  awayTeamGoals: {
+    allowNull: false,
+    type: INTEGER,
+  },
+  inProgress: {
+    allowNull: false,
+    type: BOOLEAN,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'matches',
+  timestamps: false,
+});
+
+Matche.belongsTo(Team, { foreignKey: 'homeTeam', as: 'homeT' });
+Matche.belongsTo(Team, { foreignKey: 'awayTeam', as: 'awayT' });
+
+Team.hasMany(Matche, { foreignKey: 'homeTeam', as: 'homeM' });
+Team.hasMany(Matche, { foreignKey: 'awayTeam', as: 'awayM' });
+
+export default Matche;
