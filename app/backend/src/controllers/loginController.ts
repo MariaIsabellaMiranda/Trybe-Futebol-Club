1b798ccd3adaa98be805aa9922bb979bffa7be4f
Maria Isabella Miranda da Silva <ma_isabella.miranda@hotmail.com>
2022-10-04 15:02:37 -0300

    feat: Adicionado retorno de controller loginValidate

--- app/backend/src/controllers/loginController.ts
+++ app/backend/src/controllers/loginController.ts
@@ -4,13 +4,13 @@ import LoginService from '../services/LoginService';
 export default class LoginController {
   constructor(private loginService: LoginService) { }
 
-  async loginValidate(req: Request, res: Response) {
+  async loginValidate(req: Request, res: Response): Promise<Response> {
     const { email, password } = req.body;
 
     const { code, data, message } = await this.loginService.verifyLogin({ email, password });
 
     if (!data) return res.status(code).json(message);
 
-    res.status(code).json(data);
+    return res.status(code).json(data);
   }
 }
