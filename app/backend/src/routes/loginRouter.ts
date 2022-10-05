c3c2724770e0694e0bec3fed603b401eb7720ed0
Maria Isabella Miranda da Silva <ma_isabella.miranda@hotmail.com>
2022-10-04 22:22:04 -0300

    feat/refactor: Adicionadas novas rotas e pequena refatoração para não precisar mais importar aqruivo do model

--- app/backend/src/routes/loginRouter.ts
+++ app/backend/src/routes/loginRouter.ts
@@ -1,13 +1,15 @@
 import { Router } from 'express';
 import User from '../database/models/User';
-import LoginModel from '../models/loginModel';
 import LoginService from '../services/LoginService';
 import LoginController from '../controllers/loginController';
+import validToken from '../middlewares/auth';
 
 const router = Router();
 
-const loginController = new LoginController(new LoginService(new LoginModel(User)));
+const loginService = new LoginService(User);
+const loginController = new LoginController(loginService);
 
-router.post('/', loginController.loginValidate);
+router.post('/', (req, res) => loginController.loginValidate(req, res));
+router.get('/validate', validToken, (req, res) => loginController.tokenValidate(req, res));
 
 export default router;
