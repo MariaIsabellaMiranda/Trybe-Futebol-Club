866bea618e717252209d79574280177cad484c07
Maria Isabella Miranda da Silva <ma_isabella.miranda@hotmail.com>
2022-10-04 23:17:04 -0300

    feat: Adicionada rota /teams no app

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -1,6 +1,7 @@
 import * as express from 'express';
 import errorMiddleware from './middlewares/errorMiddleware';
 import loginRouter from './routes/loginRouter';
+import teamRouter from './routes/teamRouter';
 
 class App {
   public app: express.Express;
@@ -25,6 +26,7 @@ class App {
     this.app.use(express.json());
     this.app.use(accessControl);
     this.app.use('/login', loginRouter);
+    this.app.use('/teams', teamRouter);
     this.app.use(errorMiddleware);
   }
 
