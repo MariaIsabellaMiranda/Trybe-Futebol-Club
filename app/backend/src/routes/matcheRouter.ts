39215dccfdbdd8443dcd7b57fce6b22fc40accb3
Maria Isabella Miranda da Silva <ma_isabella.miranda@hotmail.com>
2022-10-05 18:33:45 -0300

    feat: Criadas novas rotas para matches

--- app/backend/src/routes/matcheRouter.ts
+++ app/backend/src/routes/matcheRouter.ts
@@ -1,15 +1,18 @@
 import { Router } from 'express';
 import validToken from '../middlewares/auth';
 import Matche from '../database/models/Matche';
-import MatcheService from '../services/MatcheService';
 import MatcheController from '../controllers/MatcheController';
+import Team from '../database/models/Team';
+import MatcheService from '../services/MatcheService';
 
 const router = Router();
 
-const matcheService = new MatcheService(Matche);
+const matcheService = new MatcheService(Matche, Team);
 const matcheController = new MatcheController(matcheService);
 
 router.get('/', (req, res) => matcheController.getMatchesAll(req, res));
 router.post('/', validToken, (req, res) => matcheController.saveMtaches(req, res));
+router.patch('/:id/finish', (req, res) => matcheController.updateByInProgress(req, res));
+router.patch('/:id', (req, res) => matcheController.updateMatcheProgress(req, res));
 
 export default router;
