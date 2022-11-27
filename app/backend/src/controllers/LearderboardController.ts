407e35cd737c9b87726815508942a93156cfd108
Maria Isabella Miranda da Silva <ma_isabella.miranda@hotmail.com>
2022-11-27 18:49:29 -0300

    fix: [LeaderboardController] Adicionados novos métodos para controar as requisições para a rota leaderboard/away, leacderboard/home e leaderboard/

--- app/backend/src/controllers/LearderboardController.ts
+++ app/backend/src/controllers/LearderboardController.ts
@@ -4,8 +4,20 @@ import LeaderboardService from '../services/LeaderboardService';
 export default class LeaderboardController {
   constructor(private learderService: LeaderboardService) { }
 
-  async getHomeTeams(req: Request, res: Response): Promise<Response> {
+  async getHomeTeams(_req: Request, res: Response): Promise<Response> {
     const { code, data } = await this.learderService.getHomeTeams();
+
+    return res.status(code).json(data);
+  }
+
+  async getAwayTeams(_req: Request, res: Response): Promise<Response> {
+    const { code, data } = await this.learderService.getAwayTeams();
+
+    return res.status(code).json(data);
+  }
+
+  async getAllTeams(_req: Request, res: Response): Promise<Response> {
+    const { code, data } = await this.learderService.getAllTeams();
 
     return res.status(code).json(data);
   }
