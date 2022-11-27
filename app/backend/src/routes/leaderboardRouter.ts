b6d828cb002eda3083e249e1bab5ce692a48fb94
Maria Isabella Miranda da Silva <ma_isabella.miranda@hotmail.com>
2022-11-27 18:52:42 -0300

    feat: Criadas novas rotas e adicionado módulo as clases

--- app/backend/src/routes/leaderboardRouter.ts
+++ app/backend/src/routes/leaderboardRouter.ts
@@ -2,12 +2,15 @@ import { Router } from 'express';
 import LeaderboardService from '../services/LeaderboardService';
 import LeaderboardController from '../controllers/LearderboardController';
 import Team from '../database/models/Team';
+import Matche from '../database/models/Matche';
 
 const router = Router();
 
-const leaderService = new LeaderboardService(Team);
+const leaderService = new LeaderboardService(Team, Matche);
 const leaderController = new LeaderboardController(leaderService);
 
 router.get('/home', (req, res) => leaderController.getHomeTeams(req, res));
+router.get('/away', (req, res) => leaderController.getAwayTeams(req, res));
+router.get('/', (req, res) => leaderController.getAllTeams(req, res));
 
 export default router;
