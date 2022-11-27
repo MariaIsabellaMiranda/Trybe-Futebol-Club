7537dbd435af9e722481329ffac0c162e5d17423
Maria Isabella Miranda da Silva <ma_isabella.miranda@hotmail.com>
2022-11-27 18:53:31 -0300

    feat/refactor: Melhorada classe de LeaderboardService e incrementados métodos para novas rotas

--- app/backend/src/services/LeaderboardService.ts
+++ app/backend/src/services/LeaderboardService.ts
@@ -1,82 +1,13 @@
-import { IRatings, ITeams, StatusCodes } from '../entities/entities';
+import { IRatings, StatusCodes } from '../entities/entities';
 import Matche from '../database/models/Matche';
 import Team from '../database/models/Team';
+import leaderboardHomeHelper from '../helper/leaderboardHomeHelper';
+import leaderboardAwayHelper from '../helper/leaderboardAwayHelper';
+import sortRatingHelper from '../helper/sortRatingHelper';
+import leaderBoardAllHelper from '../helper/leaderBoardAllHelper';
 
 export default class LeaderboardService {
-  constructor(private teamsModel: typeof Team) { }
-  calculateTotalPoints = (teams: any) => {
-    let wins = 0;
-    let loss = 0;
-    let draw = 0;
-    let totalPoints = 0;
-
-    teams.homeM.forEach((matches: any) => {
-      if (matches.homeTeamGoals > matches.awayTeamGoals) {
-        wins += 1;
-        totalPoints += 3;
-      } else if (matches.homeTeamGoals < matches.awayTeamGoals) {
-        loss += 1;
-        totalPoints += 0;
-      } else if (matches.homeTeamGoals === matches.awayTeamGoals) {
-        draw += 1;
-        totalPoints += 1;
-      }
-    });
-
-    return { wins, loss, draw, totalPoints };
-  };
-
-  calculateGoals = (teams: ITeams) => {
-    let goalsFavor = 0;
-    let goalsOwn = 0;
-
-    teams.homeM?.forEach((matches) => {
-      goalsFavor += matches.homeTeamGoals;
-      goalsOwn += matches.awayTeamGoals;
-    });
-
-    const goalsBalance = goalsFavor - goalsOwn;
-
-    return { goalsBalance, goalsFavor, goalsOwn };
-  };
-
-  totalRating = (teams: any) => {
-    const points = this.calculateTotalPoints(teams);
-    const goals = this.calculateGoals(teams);
-    const totalGames = teams.homeM.length;
-    const efficiency = (points.totalPoints / (totalGames * 3)) * 100;
-
-    return {
-      name: teams.teamName,
-      totalPoints: points.totalPoints,
-      totalGames,
-      totalVictories: points.wins,
-      totalDraws: points.draw,
-      totalLosses: points.loss,
-      goalsFavor: goals.goalsFavor,
-      goalsOwn: goals.goalsOwn,
-      goalsBalance: goals.goalsBalance,
-      efficiency: Number(efficiency.toFixed(2)),
-    };
-  };
-
-  sortRating = (ratings: IRatings[]) => {
-    const sortRating = ratings.sort((ratingsA: IRatings, ratingsB: IRatings) => {
-      if (ratingsA.totalPoints < ratingsB.totalPoints) { return 1; }
-      if (ratingsA.totalPoints > ratingsB.totalPoints) { return -1; }
-      if (ratingsA.totalVictories < ratingsB.totalVictories) { return 1; }
-      if (ratingsA.totalVictories > ratingsB.totalVictories) { return -1; }
-      if (ratingsA.goalsBalance < ratingsB.goalsBalance) { return 1; }
-      if (ratingsA.goalsBalance > ratingsB.goalsBalance) { return -1; }
-      if (ratingsA.goalsFavor < ratingsB.goalsFavor) { return 1; }
-      if (ratingsA.goalsFavor > ratingsB.goalsFavor) { return -1; }
-      if (ratingsA.goalsOwn < ratingsB.goalsOwn) { return 1; }
-      if (ratingsA.goalsOwn > ratingsB.goalsOwn) { return -1; }
-      return 0;
-    });
-    return sortRating;
-  };
-
+  constructor(private teamsModel: typeof Team, private matchesModel: typeof Matche) { }
   async getHomeTeams() {
     const data = await this.teamsModel.findAll({
       include: [{
@@ -85,8 +16,34 @@ export default class LeaderboardService {
 
     });
 
-    const ratings: IRatings[] = data.map((teams) => this.totalRating(teams));
-    const sortRating = this.sortRating(ratings);
+    const ratings: IRatings[] = data.map((teams) => leaderboardHomeHelper.totalRatingHome(teams));
+    const sortRating = sortRatingHelper.sortRating(ratings);
+
+    return { code: StatusCodes.ok, data: sortRating };
+  }
+
+  async getAwayTeams() {
+    const data = await this.teamsModel.findAll({
+      include: [{
+        model: Matche, as: 'awayM', where: { inProgress: false }, attributes: { exclude: ['id'] },
+      }],
+    });
+
+    const ratings: IRatings[] = data.map((teams) => leaderboardAwayHelper.totalRatingAway(teams));
+    const sortRating = sortRatingHelper.sortRating(ratings);
+
+    return { code: StatusCodes.ok, data: sortRating };
+  }
+
+  async getAllTeams() {
+    const { data: homeMatches } = await this.getHomeTeams();
+    const { data: awayMatches } = await this.getAwayTeams();
+
+    sortRatingHelper.sortTeams(homeMatches);
+    sortRatingHelper.sortTeams(awayMatches);
+
+    const summedScores = leaderBoardAllHelper.sumScores(homeMatches, awayMatches);
+    const sortRating = sortRatingHelper.sortRating(summedScores);
 
     return { code: StatusCodes.ok, data: sortRating };
   }
