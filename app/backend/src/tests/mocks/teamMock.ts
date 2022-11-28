84cb15c2c0ef83323291f3eeab3e4c980505d5f1
Maria Isabella Miranda da Silva <ma_isabella.miranda@hotmail.com>
2022-11-28 00:47:31 -0300

    feat: [teamMock] Criados mocks para serem usados nos testes da rota teams.

---
+++ app/backend/src/tests/mocks/teamMock.ts
@@ -0,0 +1,19 @@
+export const dumpTeams = [
+  {
+    "id": 1,
+    "teamName": "Avaí/Kindermann"
+  },
+  {
+    "id": 2,
+    "teamName": "Bahia"
+  },
+  {
+    "id": 3,
+    "teamName": "Botafogo"
+  }
+]
+
+export const dumpTeam = {
+  "id": 5,
+  "teamName": "Cruzeiro"
+}
