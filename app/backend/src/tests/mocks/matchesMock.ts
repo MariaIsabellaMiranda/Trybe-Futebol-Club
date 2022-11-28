da48dc6d57f816f9e84f60ce7247cc30f69bd364
Maria Isabella Miranda da Silva <ma_isabella.miranda@hotmail.com>
2022-11-28 00:47:12 -0300

    feat: [matchesMock] Criados mocks para serem usados nos testes da rota matches.

---
+++ app/backend/src/tests/mocks/matchesMock.ts
@@ -0,0 +1,37 @@
+export const dumpAllMatches = [{
+  id: 1,
+  homeTeam: 16,
+  homeTeamGoals: 1,
+  awayTeam: 8,
+  awayTeamGoals: 1,
+  inProgress: false,
+  teamHome: {
+      teamName: "São Paulo"
+  },
+  teamAway: {
+      teamName: "Grêmio"
+  }
+}];
+
+export const dumpMatche = {
+  id: 1,
+  homeTeam: 16,
+  awayTeam: 8,
+  homeTeamGoals: 1,
+  awayTeamGoals: 1,
+  inProgress: false,
+};
+
+export const dumpMatcheWithTeamsEquals = {
+  id: 1,
+  homeTeam: 16,
+  awayTeam: 16,
+  homeTeamGoals: 1,
+  awayTeamGoals: 1,
+  inProgress: false,
+};
+
+export const dumpMatcheGoals = {
+  homeTeamGoals: 7,
+  awayTeamGoals: 1,
+}
