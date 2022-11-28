8107e7a93dabbba9b6d3e6f7ea07d083f506ea07
Maria Isabella Miranda da Silva <ma_isabella.miranda@hotmail.com>
2022-11-28 00:45:31 -0300

    test: [team.test] Implementados testes para rota /teams.

--- app/backend/src/tests/team.test.ts
+++ app/backend/src/tests/team.test.ts
@@ -1,4 +1,4 @@
-import { dumpTeams, dumpTeam } from './teamMock';
+import { dumpTeams, dumpTeam } from './mocks/teamMock';
 import * as sinon from 'sinon';
 import * as chai from 'chai';
 // @ts-ignore
@@ -7,41 +7,67 @@ import chaiHttp = require('chai-http');
 import { app } from '../app';
 import Team from '../database/models/Team';
 
-import { Response } from 'superagent';
-
 chai.use(chaiHttp);
 
 const { expect } = chai;
 
 describe('Testa as funcionalidades na rota /teams', () => {
-  let chaiHttpResponse: Response;
-
   beforeEach(async () => {
     sinon.stub(Team, "findAll").resolves( dumpTeams as Team[]);
-    sinon.stub(Team, "findByPk").resolves( dumpTeam as Team);
-
   });
 
   afterEach(()=>{
    (Team.findAll as sinon.SinonStub).restore();
-   (Team.findByPk as sinon.SinonStub).restore();
   })
 
   it('se retorna todos os teams', async () => {
-    chaiHttpResponse = await chai
+    const chaiHttpResponse = await chai
       .request(app)
       .get('/teams');
 
       expect(chaiHttpResponse.status).to.equal(200);
       expect(chaiHttpResponse.body).to.deep.equal(dumpTeams);
   });
+});
 
-  it('se retorna apenas o team cujo id foi passado por params', async () => {
-    chaiHttpResponse = await chai
-      .request(app)
-      .get('/teams/5');
+describe('Testa as funcionalidades na rota /teams/:id', () => {
+  describe('Testa o caso de sucesso', () => {
+    beforeEach(async () => {
+      sinon.stub(Team, "findByPk").resolves( dumpTeam as Team);
+  
+    });
+  
+    afterEach(()=>{
+     (Team.findByPk as sinon.SinonStub).restore();
+    })
+  
+    it('se retorna apenas o team cujo id foi passado por params', async () => {
+      const chaiHttpResponse = await chai
+        .request(app)
+        .get('/teams/5');
+  
+        expect(chaiHttpResponse.status).to.equal(200);
+        expect(chaiHttpResponse.body).to.deep.equal(dumpTeam);
+    });
+  });
 
-      expect(chaiHttpResponse.status).to.equal(200);
-      expect(chaiHttpResponse.body).to.deep.equal(dumpTeam);
+  describe('Testa o caso de erro', () => {
+    beforeEach(async () => {
+      sinon.stub(Team, "findByPk").resolves(null);
+  
+    });
+  
+    afterEach(()=>{
+     (Team.findByPk as sinon.SinonStub).restore();
+    })
+  
+    it('se retorna um erro quando o time não é encontrado', async () => {
+      const chaiHttpResponse = await chai
+        .request(app)
+        .get('/teams/5');
+  
+        expect(chaiHttpResponse.status).to.equal(404);
+        expect(chaiHttpResponse.body).to.equal('Not Found');
+    });
   });
 });
