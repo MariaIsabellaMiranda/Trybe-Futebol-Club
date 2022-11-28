0bfbe04027dbd396aa4331624a954997127757c8
Maria Isabella Miranda da Silva <ma_isabella.miranda@hotmail.com>
2022-11-28 00:46:09 -0300

    refactor: Arquivos de mock movidos para pasta mocks.

--- app/backend/src/tests/userMock.ts
+++
@@ -1,9 +0,0 @@
-export const dumpUser = {
-  id: 1,
-  username: 'Admin',
-  role: 'admin',
-  email: 'admin@admin.com',
-  password: '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW'
-}
-
-export const dumpToken = 'eyJhbGciOiJIUzI1NiJ9.YWRtaW5AYWRtaW4uY29t.s1U6I8B6x_9eLeJyb9PdjTz1JbNXo57xor-T1493RW0';
