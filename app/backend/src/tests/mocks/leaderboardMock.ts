65b336bef61bc0afa6d480f48748434ab1d6a2e9
Maria Isabella Miranda da Silva <ma_isabella.miranda@hotmail.com>
2022-11-28 00:46:50 -0300

    feat: [leaderboardMock] Criados mocks para serem usados nos testes da rota leaderboard.

---
+++ app/backend/src/tests/mocks/leaderboardMock.ts
@@ -0,0 +1,66 @@
+export const dumpTeamsHome = [{
+  id: 1,
+  teamName: 'Santos',
+  homeM: [
+    {
+      id: 2,
+      homeTeam: 5,
+      homeTeamGoals: 5,
+      awayTeam: 2,
+      awayTeamGoals: 0,
+      inProgress: false
+    } ]
+}];
+
+export const dumpTeamsAway = [{
+  id: 2,
+  teamName: 'Santos',
+  awayM: [
+    {
+      id: 1,
+      homeTeam: 1,
+      homeTeamGoals: 2,
+      awayTeam: 2,
+      awayTeamGoals: 1,
+      inProgress: false
+    } ]
+}];
+
+export const dumpAllRatings = [{
+  name: 'Santos',
+  totalPoints: 3,
+  totalGames: 2,
+  totalVictories: 1,
+  totalDraws: 0,
+  totalLosses: 1,
+  goalsFavor: 6,
+  goalsOwn: 2,
+  goalsBalance: 4,
+  efficiency: 50
+}];
+
+export const dumpHomeRatings = [{
+  name: 'Santos',
+  totalPoints: 3,
+  totalGames: 1,
+  totalVictories: 1,
+  totalDraws: 0,
+  totalLosses: 0,
+  goalsFavor: 5,
+  goalsOwn: 0,
+  goalsBalance: 5,
+  efficiency: 100
+}];
+
+export const dumpAwayRatings = [{
+  name: 'Santos',
+  totalPoints: 0,
+  totalGames: 1,
+  totalVictories: 0,
+  totalDraws: 0,
+  totalLosses: 1,
+  goalsFavor: 1,
+  goalsOwn: 2,
+  goalsBalance: -1,
+  efficiency: 0
+}];
